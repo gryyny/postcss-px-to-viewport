ef9b1cc844edb56af5b2b992935c97ab18de3a80
Andryi Grinchuck <gryyny@gmail.com>
2017-09-12 12:45:44 +0300

    add media query based viewportWidth support

--- index.js
+++ index.js
@@ -12,60 +12,75 @@ var objectAssign = require('object-assign');
 var pxRegex = /"[^"]+"|'[^']+'|url\([^\)]+\)|(\d*\.?\d+)px/ig;
 
 var defaults = {
-  viewportWidth: 320,
-  viewportHeight: 568, // not now used; TODO: need for different units and math for different properties
-  unitPrecision: 5,
-  viewportUnit: 'vw',
-  selectorBlackList: [],
-  minPixelValue: 1,
-  mediaQuery: false
+    viewportWidth: 1280,
+    mobMediaParams: {'mediaRule':'only screen and (max-width: 800px)', 'viewportWidth': 320},
+    viewportHeight: 568, // not now used; TODO: need for different units and math for different properties
+    unitPrecision: 5,
+    viewportUnit: 'vw',
+    selectorBlackList: [],
+    minPixelValue: 1,
+    mediaQuery: false
 };
 
 module.exports = postcss.plugin('postcss-px-to-viewport', function (options) {
 
-  var opts = objectAssign({}, defaults, options);
-  var pxReplace = createPxReplace(opts.viewportWidth, opts.minPixelValue, opts.unitPrecision, opts.viewportUnit);
+    var opts = objectAssign({}, defaults, options);
+    var pxReplace = createPxReplace(opts.viewportWidth, opts.minPixelValue, opts.unitPrecision, opts.viewportUnit);
 
-  return function (css) {
+    return function (css) {
 
-    css.walkDecls(function (decl, i) {
-      // This should be the fastest test and will remove most declarations
-      if (decl.value.indexOf('px') === -1) return;
+        css.walkAtRules('media', function (rule) {
+            if (rule.params.indexOf(opts.mobMediaParams.mediaRule) !== -1) {
+                var mobPxReplace = createPxReplace(opts.mobMediaParams.viewportWidth, opts.minPixelValue, opts.unitPrecision, opts.viewportUnit);
+                rule.walkDecls(function (decl, i) {
+                    // This should be the fastest test and will remove most declarations
+                    if (decl.value.indexOf('px') === -1) return;
 
-      if (blacklistedSelector(opts.selectorBlackList, decl.parent.selector)) return;
+                    if (blacklistedSelector(opts.selectorBlackList, decl.parent.selector)) return;
 
-      decl.value = decl.value.replace(pxRegex, pxReplace);
-    });
+                    decl.value = decl.value.replace(pxRegex, mobPxReplace);
+                });
+            }
+        });
 
-    if (opts.mediaQuery) {
-      css.walkAtRules('media', function (rule) {
-        if (rule.params.indexOf('px') === -1) return;
-        rule.params = rule.params.replace(pxRegex, pxReplace);
-      });
-    }
+        css.walkDecls(function (decl, i) {
+            // This should be the fastest test and will remove most declarations
+            if (decl.value.indexOf('px') === -1) return;
 
-  };
+            if (blacklistedSelector(opts.selectorBlackList, decl.parent.selector)) return;
+
+            decl.value = decl.value.replace(pxRegex, pxReplace);
+        });
+
+        if (opts.mediaQuery) {
+            css.walkAtRules('media', function (rule) {
+                if (rule.params.indexOf('px') === -1) return;
+                rule.params = rule.params.replace(pxRegex, pxReplace);
+            });
+        }
+
+    };
 });
 
 function createPxReplace(viewportSize, minPixelValue, unitPrecision, viewportUnit) {
-  return function (m, $1) {
-    if (!$1) return m;
-    var pixels = parseFloat($1);
-    if (pixels <= minPixelValue) return m;
-    return toFixed((pixels / viewportSize * 100), unitPrecision) + viewportUnit;
-  };
+    return function (m, $1) {
+        if (!$1) return m;
+        var pixels = parseFloat($1);
+        if (pixels <= minPixelValue) return m;
+        return toFixed((pixels / viewportSize * 100), unitPrecision) + viewportUnit;
+    };
 }
 
 function toFixed(number, precision) {
-  var multiplier = Math.pow(10, precision + 1),
-    wholeNumber = Math.floor(number * multiplier);
-  return Math.round(wholeNumber / 10) * 10 / multiplier;
+    var multiplier = Math.pow(10, precision + 1),
+        wholeNumber = Math.floor(number * multiplier);
+    return Math.round(wholeNumber / 10) * 10 / multiplier;
 }
 
 function blacklistedSelector(blacklist, selector) {
-  if (typeof selector !== 'string') return;
-  return blacklist.some(function (regex) {
-    if (typeof regex === 'string') return selector.indexOf(regex) !== -1;
-    return selector.match(regex);
-  });
+    if (typeof selector !== 'string') return;
+    return blacklist.some(function (regex) {
+        if (typeof regex === 'string') return selector.indexOf(regex) !== -1;
+        return selector.match(regex);
+    });
 }
